01288e22293593434b578f672a3f6d33f8353dc7
mahmoudmousahamad <mahmoudmousahamad@gmail.com>
2022-03-24 15:48:42 -0500

    .

--- Project_4_Problems/Problem_1/hash_kernel.cu
+++ Project_4_Problems/Problem_1/hash_kernel.cu
@@ -6,11 +6,7 @@
 __global__
 unsigned int my_generate_hash(unsigned int nonce, unsigned int index, unsigned int* transactions, unsigned int n_transactions) {
 
-    unsigned int hash = (nonce + transactions[0] * (index + 1)) % MAX;
-    for(int j = 1; j < n_transactions; j++){
-        hash = (hash + transactions[j] * (index + 1)) % MAX;
-     }
-     return hash; 
+    
 
 } // End Generate Hash ---------- //
 
@@ -18,13 +14,17 @@ unsigned int my_generate_hash(unsigned int nonce, unsigned int index, unsigned i
 *       Generates an array of hash values from nonces.
 */
 __global__
-void hash_kernel(unsigned int* hash_array, unsigned int* nonce_array, unsigned int array_size, unsigned int* transactions, unsigned int n_transactions, unsigned int mod) {
+void hash_kernel(unsigned int* hash_array, unsigned int* nonce, unsigned int array_size, unsigned int* transactions, unsigned int n_transactions, unsigned int mod) {
 
     // Calculate thread index
     unsigned int index = blockDim.x * blockIdx.x + threadIdx.x;
 
     if (index < array_size) {
-        hash_array[index] = my_generate_hash(nonce_array[index], index, transactions, n_transactions) % mod;
+        unsigned int hash = (nonce + transactions[0] * (index + 1)) % mod;
+        for(int j = 1; j < n_transactions; j++){
+            hash = (hash + transactions[j] * (index + 1)) % mod;
+        }
+        hash_array[index] = hash;
     }
 
 } // End Hash Kernel //
