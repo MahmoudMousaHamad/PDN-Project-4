8a723a58289f359c99667000db25fead1d0b753c
mahmoudmousahamad <mahmoudmousahamad@gmail.com>
2022-03-27 15:01:52 -0500

    .

--- Project_4_Problems/Problem_2/reduction_kernel.cu
+++ Project_4_Problems/Problem_2/reduction_kernel.cu
@@ -30,6 +30,7 @@ void reduction_kernel(unsigned int* hash_array, unsigned int* nonce_array, unsig
 
     if ((index + BLOCK_SIZE) < array_size) {
         if (hash_array[index + BLOCK_SIZE] < hash_reduction[threadIdx.x]) {
+            printf("DEBUG: %d\n", hash_array[index + BLOCK_SIZE]);
             hash_reduction[threadIdx.x] = hash_array[index + BLOCK_SIZE];
             nonce_reduction[threadIdx.x] = nonce_array[index + BLOCK_SIZE];
         }
@@ -44,7 +45,6 @@ void reduction_kernel(unsigned int* hash_array, unsigned int* nonce_array, unsig
     }
 
     if (threadIdx.x == 0) {
-        printf("DEBUG: %d\n", nonce_reduction[0]);
         *out_hash = hash_reduction[0];
         *out_nonce = nonce_reduction[0];
     }
