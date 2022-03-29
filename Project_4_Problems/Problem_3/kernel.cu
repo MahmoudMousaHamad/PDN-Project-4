b336e728d260b9c26fdacae7f6503cd73c3ec5da
mahmoudmousahamad <mahmoudmousahamad@gmail.com>
2022-03-29 15:05:30 -0500

    .

--- Project_4_Problems/Problem_3/kernel.cu
+++ Project_4_Problems/Problem_3/kernel.cu
@@ -29,6 +29,7 @@ void convolution_kernel(int* A, int* K, int* out, int w, int h) {
             }
         }
         __syncthreads();
+        printf("%d, " pixVal);
         out[Row * w + Col] = pixVal;
     }
 }
