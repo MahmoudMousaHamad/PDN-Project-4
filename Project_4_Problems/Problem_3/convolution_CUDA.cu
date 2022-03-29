83d1022a1ee3698358641287b2f16fa9db6af4f0
mahmoudmousahamad <mahmoudmousahamad@gmail.com>
2022-03-29 14:06:21 -0500

    .

--- Project_4_Problems/Problem_3/convolution_CUDA.cu
+++ Project_4_Problems/Problem_3/convolution_CUDA.cu
@@ -6,24 +6,11 @@
 #include <curand_kernel.h>
 #include <driver_types.h>
 #include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include <time.h>
 #include <cstdio>
 #include <cuda.h>
 #include <iostream>
-#include <cuda_runtime_api.h>
-#include <curand_kernel.h>
-#include <driver_types.h>
 #include <curand.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include <time.h>
-#include <cstdio>
-#include <cuda.h>
+
 
 #include "kernel.cu"
 
