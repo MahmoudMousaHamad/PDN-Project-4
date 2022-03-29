d71be625393082b6074b117f398d67bca530c18b
mahmoudmousahamad <mahmoudmousahamad@gmail.com>
2022-03-29 14:26:20 -0500

    .

--- Project_4_Problems/run.sh
+++ Project_4_Problems/run.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+TESTS="../test_data"
 TRANSACTIONS="../test_data/Problem 1 and 2/debug_1k.csv"
 N_TRANSACTIONS="1000"
 
 P1="./Problem_1"
 P2="./Problem_2"
+P3="./Problem_3"
+P4="./Problem_4"
+
 
 P1_RESULTS="./results/p1_results"
 P2_RESULTS="./results/p2_results"
+P3_RESULTS="./results/p3_results"
+P4_RESULTS="./results/p4_results"
 
 mkdir -p $P1_RESULTS $P2_RESULTS
 
@@ -56,5 +62,16 @@ problem_2() {
 }
 
 problem_3() {
-    
+    echo "################ Problem - 3 ################"
+
+    make -C ./Problem_3/
+
+    test_dir="$TESTS/Problem\ 3/"
+    out="$P3_RESULTS/output_convolution.csv"
+    time="$P3_RESULTS/time.csv"
+    dimension=2048
+
+    $P3/convolution_serial $dimension $dimension $test_dir/mat_input.csv $out $time
 }
+
+problem_3
